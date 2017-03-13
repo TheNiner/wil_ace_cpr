5b6374d4989b4817abf364605cfd93876df2040f
Niner1309 <sven0215@hotmail.com>
2017-03-13 20:30:32 +0100

    added keys and mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "Willard's ACE CPR Fix";
+actionName = "GITHUB";
+action = "https://github.com/TheNiner/wil_ace_cpr";
+description = "wil_ace_cpr - Version 1.0";
+tooltip = "wil_ace_cpr";
+tooltipOwned = "Owned by Willard";
+overview = "When using the ACE advanced medical system in combination with the revive system, some players won't be able to get revived without a PAK. This small mod changes the behavior of the CPR function in such a way, that as long as the player has a certain blood volume, he can get revived.";
+author = "Willard";
